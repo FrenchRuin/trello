cde4189fd0561c76bff5f93a50b9a59f7c93ce7a
Dev_Lee <toxic023@naver.com>
2024-01-01 14:23:57 +0900

    auth && org

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,11 @@
+import { OrganizationList } from '@clerk/nextjs'
+
+export default function CreateOrganization() {
+	return (
+		<OrganizationList
+			hidePersonal
+			afterCreateOrganizationUrl={'/organization/:id'}
+			afterSelectOrganizationUrl={'/organization/:id'}
+		/>
+	)
+}
